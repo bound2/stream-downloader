fd7430a9dc0a116d84dc15dc26e649c8b0b182b1
bound2 <9380102+bound2@users.noreply.github.com>
2017-03-20 00:04:42 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,7 +6,8 @@ echo "Beginning to download the greatest TV show ever"
 #ep3 - https://mtg-vod-http.babahhcdn.com/mtg/_definst_/smil:tv3/prooviabielu/season1/20388084/20388084_HD.smil/media_b1096000_sleng_${counter}.ts
 #ep4 - https://mtg-vod-http.babahhcdn.com/mtg/_definst_/smil:tv3/prooviabielu/season1/20388085/20388085_HD.smil/media_b1096000_sleng_${counter}.ts
 while [[ true ]]; do
-  wget -q "https://mtg-vod-http.babahhcdn.com/mtg/_definst_/smil:tv3/prooviabielu/season1/20388082/20388082e_HD.smil/media_b1096000_sleng_${counter}.ts"
+  current_time_nanos=$(ruby -e 'puts Time.now.to_f')
+  wget -q "https://mtg-vod-http.babahhcdn.com/mtg/_definst_/smil:tv3/prooviabielu/season1/20388083/20388083_HD.smil/media_b1096000_sleng_${counter}.ts" -O ${current_time_nanos}.ts
   if [ $? -ne 0 ];then
     break
   else
@@ -20,7 +21,7 @@ echo "Generating whole episode"
 
 timestamp=$(date +%s)
 cat *.ts > all.ts
-ffmpeg -i all.ts -acodec copy -vcodec copy ${timestamp}.mp4
+ffmpeg -i all.ts -acodec copy -vcodec copy ${timestamp}-final.mp4
 echo "Cleaning up partial files"
 rm -rf *.ts
 
